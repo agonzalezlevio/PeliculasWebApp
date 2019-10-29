d9f39afe5d17e20d55b2437fb9146c4f035b8505
axlfrost <ale.gonzalezlevio@gmail.com>
2019-10-28 21:16:16 -0300

    Se añaden los componentes: Película, home y navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PeliculaComponent } from './components/pelicula/pelicula.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PeliculaComponent
   ],
   imports: [
     BrowserModule,
