630056cc59ae09f1926218e3289c17acf2ab1f3e
axlfrost <ale.gonzalezlevio@gmail.com>
2019-10-28 21:16:16 -0300

    Actualización servicio peliculas

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map,delay } from 'rxjs/Operators';
 
 
 @Injectable({
@@ -16,6 +17,19 @@ export class PeliculasService {
 
   public getPopulares() {
     const url = `${ this.URL }/discover/movie?sort_by=popularity.desc&api_key=${ this.API_KEY }&language=es`;
-    return this.httpClient.get(url);
+    return this.httpClient.get(url).pipe(map((data: any) => {
+
+      const resultadoPopulares = [];
+
+      for (const pelicula of data.results) {
+        pelicula.backdrop_path = `${this.URL_IMAGE}${pelicula.backdrop_path}`;
+        pelicula.poster_path = `${this.URL_IMAGE}${pelicula.poster_path}`;
+        resultadoPopulares.unshift(pelicula);
+
+        if (resultadoPopulares.length > 5 ) { break; }
+      }
+
+      return resultadoPopulares;
+    }));
   }
 }
