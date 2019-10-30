401986dd92a7092c17c2d92232b02f9b38aaba5b
axlfrost <ale.gonzalezlevio@gmail.com>
2019-10-29 23:52:32 -0300

    Actualización servicio películas (Obtener películas populares de niños)

--- src/app/services/peliculas.service.ts
+++ src/app/services/peliculas.service.ts
@@ -10,23 +10,19 @@ export class PeliculasService {
 
   private API_KEY = 'b9ce5164f948062a1fc5362ee2dc9ca5';
   private URL = 'https://api.themoviedb.org/3';
-  private URL_IMAGE = 'https://image.tmdb.org/t/p/w500';
 
 
   constructor(private httpClient: HttpClient) { }
 
   public getPopulares() {
     const url = `${ this.URL }/discover/movie?sort_by=popularity.desc&api_key=${ this.API_KEY }&language=es`;
+
     return this.httpClient.get(url).pipe(map((data: any) => {
 
       const resultadoPopulares = [];
 
       for (const pelicula of data.results) {
-        pelicula.backdrop_path = `${this.URL_IMAGE}${pelicula.backdrop_path}`;
-        pelicula.poster_path = `${this.URL_IMAGE}${pelicula.poster_path}`;
         resultadoPopulares.unshift(pelicula);
-
-        if (resultadoPopulares.length > 5 ) { break; }
       }
 
       return resultadoPopulares;
@@ -45,22 +41,32 @@ export class PeliculasService {
 
     const desde = new Date();
     const hasta = new Date();
-    hasta.setDate(hasta.getDate() + 7 );
+    hasta.setDate(hasta.getDate() + 30 );
 
     const desdeStr = this.getFechaString(desde);
     const hastaStr = this.getFechaString(hasta);
 
 
     const url = `${ this.URL }/discover/movie?primary_release_date.gte=${desdeStr}&primary_release_date.lte=${hastaStr}&sort_by=popularity.desc&api_key=${ this.API_KEY }&language=es`;
+
+    return this.httpClient.get(url).pipe(map((data: any) => {
+      const resultadoPopulares = [];
+
+      for (const pelicula of data.results) {
+        resultadoPopulares.unshift(pelicula);
+      }
+      return resultadoPopulares;
+    }));
+  }
+
+  public getPopularesNinos() {
+    const url = `${ this.URL }/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=${ this.API_KEY }&language=es`;
     return this.httpClient.get(url).pipe(map((data: any) => {
 
       const resultadoPopulares = [];
 
       for (const pelicula of data.results) {
-        pelicula.backdrop_path = `${this.URL_IMAGE}${pelicula.backdrop_path}`;
-        pelicula.poster_path = `${this.URL_IMAGE}${pelicula.poster_path}`;
         resultadoPopulares.unshift(pelicula);
-        if (resultadoPopulares.length > 5 ) { break; }
       }
       return resultadoPopulares;
     }));
