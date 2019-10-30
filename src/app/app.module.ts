93ebd0d4e930a90a82d27919175a50e2de0b4063
axlfrost <ale.gonzalezlevio@gmail.com>
2019-10-29 21:16:16 -0300

    Se añaden las rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { PeliculaComponent } from './components/pelicula/pelicula.component';
+import { BuscarComponent } from './components/buscar/buscar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    PeliculaComponent
+    PeliculaComponent,
+    BuscarComponent
   ],
   imports: [
     BrowserModule,
